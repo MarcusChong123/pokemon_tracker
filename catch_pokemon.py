e34c12a5db602815900c39f4270babe96c28dce4
Marcus Chong <kok_yung@hotmail.com>
2021-05-17 13:40:43 +0800

    Added fifth pokemon, Butterfree

--- catch_pokemon.py
+++ catch_pokemon.py
@@ -5,6 +5,7 @@ def main():
     print('This is our second pokemon, Charmander')
     print('This is our third pokemon, Mewtwo')
     print('This is our fourth pokemon, Snorlax')
+    print('This is our fifth pokemon, Butterfree')
     print(datetime.now())
 
 main()
