d3d15324e7da4a7eb6670636a4b3a9241f6608b9
Marcus Chong <44716256+MarcusChong123@users.noreply.github.com>
2021-05-17 13:36:49 +0800

    Add datetime now to print current time

--- catch_pokemon.py
+++ catch_pokemon.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 
 def main():
     print('This is our first pokemon, Pikachu!')
     print('This is our second pokemon, Charmander')
     print('This is our third pokemon, Mewtwo')
     print('This is our fourth pokemon, Snorlax')
+    print(datetime.now())
 
 main()
