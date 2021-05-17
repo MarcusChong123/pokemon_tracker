7d39b72900174f8059d9625afec8b6dc71c4db1d
Marcus Chong <kok_yung@hotmail.com>
2021-05-17 11:56:04 +0800

    Add a new file 'get_all_pokemon.py'
    This file will request for 151 pokemon names

--- get_all_pokemon.py
+++ get_all_pokemon.py
@@ -1,3 +1,11 @@
+import requests
 from datetime import datetime
 
+def main():
+    for i in range(1, 151):
+        pokemon_req = requests.get(f'https://pokeapi.co/api/v2/pokemon/{i}/')
+        pokemon_req = pokemon_req.json()
+        print (pokemon_req['name'])
+
+main()
 print (datetime.now())
